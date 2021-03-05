e179a8704df50d38e89afcf8b1e6d98964d57284
github username <github email address>
2021-03-05 13:09:09 -0600

    started activities controller and nested routes in config routes;

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,4 +1,32 @@
 class Api::V1::ActivitiesController < ApplicationController
 
+    before_action :set_trip
+
+    def index
+      render json: @trip.activities
+    end
+  
+    def create
+        
+    end
+  
+    def show
+        
+    end
+  
+    def destroy
+       
+    end
+  
+      private
+
+    def set_trip
+      @trip = Trip.find(params[:trip_id])  
+    end
+    
+  
+    def activity_params
+        params.require(:activity).permit(:trip_id, :place, :kind, :description, :date)
+     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :activities
-      resources :trips
+      resources :trips do
+        resources :activities
+      end
     end
-  end
+  end 
 
 end
