98b7aaa23f4a349412232974cdb06e98eaf3576c
github username <github email address>
2021-03-11 09:48:21 -0600

    sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SessionsController < ApplicationController
+    #ask for clarification on when to use a sessions controller vs just handling all this logic in users controller
+    def create
+      @user = User.find_by(username: params[:session][:username])
+      session[:user_id] = @user.id
+      
+      @user && @user.authenticate(params[:session][:password]) ? render json: @user, include: ['trips.users'] : render json: {error: 'Invalid Credentials'}
+      #check on this
+    end
+
+    def get_current_user
+        logged_in? ? render json: current_user : render json {error: 'Please log in'}
+        #check on this
+    end
+
+    def destroy
+    session.clear
+    end
+end 
