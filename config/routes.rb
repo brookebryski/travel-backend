5b9fb5ff6ba3a5d14ac1350f558377b8a87740d0
github username <github email address>
2021-03-11 13:08:25 -0600

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
-  
-  namespace :api do
-    namespace :v1 do
-      resources :trips do
-        resources :activities
-      end
-    end
-  end 
 
-end
+  post "api/v1/login", to: "api/v1/sessions#create"
+  get "api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  delete "api/v1/logout", to: "api/v1/sessions#destroy"
+  post "/api/v1/signup", to: "api/v1/users#create"
+
+  
+  namespace :api do 
+    namespace :v1 do 
+      
+      resources :trips do 
+        resources :users
+      end 
+
+      resources :users do 
+        # resources :toy_ownerships
+        resources :trips 
+      end 
+       
+      resources :trip_histories
+
+    end 
+  end 
+ end
