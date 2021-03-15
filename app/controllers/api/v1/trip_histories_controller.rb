cb3797f3a9fa71f43e34987376b1601b2f13e544
github username <github email address>
2021-03-15 12:35:59 -0500

    debugged trip histories controller

--- app/controllers/api/v1/trip_histories_controller.rb
+++ app/controllers/api/v1/trip_histories_controller.rb
@@ -1,45 +1,56 @@
 class Api::V1::TripHistoriesController < ApplicationController
-    before_action :set_trip_history, only: [:show, update, :destroy]
 
-      def index
-        @trip_histories = TripHistory.all
-        render json: @trip_histories
-      end
+  before_action :set_trip_history, only: [:show, :update, :destroy]
 
-      def show 
-        render json: @trip_history
-      end
 
-      def create
-        @trip_history = TripHistory.new(trip_history_params)
-        if @trip_history.save
-          render json: @trip_history
-        else
-          render json: {message: @trip_history.errors }, status: 400
-          #how does this work?
-      end
+  # GET /trip_histories
+  def index
+    @trip_histories = TripHistory.all
+
+    render json: @trip_histories
+  end
+
+  # GET /trip_histories/1
+  def show
+    render json: @trip_history
+  end
+
+  # POST /trip_histories
+  def create
+    @trip_history = TripHistory.new(trip_history_params)
+    if @trip_history.save
+      render json: @trip_history
+    else
+      render json: {message: @trip_history.errors }, status: 400
     end
+  end
 
-      def update
-        if @trip_history.update(trip_history_params)
-          render json: @trip_history
-        else
-          render json: @trip_history.errors, status: :unprocessable_entity
-           #how does this work?
-      end
+
+  # PATCH/PUT /trip_histories/1
+  def update
+    if @trip_history.update(trip_history_params)
+      render json: @trip_history
+    else
+      render json: @trip_history.errors, status: :unprocessable_entity
     end
+  end
 
-      def destroy
-        @trip_history.destroy
-      end
+  # DELETE /trip_histories/1
+  def destroy
+    @trip_history.destroy
+  end
 
-    private
 
-    def set_trip_history
-      @trip_history = TripHistory.find(params[:id])
-    end
 
-    def trip_history_params
-      params.require(:trip_history).permit(:trip_id, :user_id)
-    end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_trip_history
+    @trip_history = TripHistory.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def trip_history_params
+    params.require(:trip_history).permit(:trip_id, :user_id)
+  end
+
 end
